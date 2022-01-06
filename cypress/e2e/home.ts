8a017b3311019a39f835dff4ec4fb239297a6eb2
Morpa <andremorpa@gmail.com>
2022-01-06 21:36:30 +0000

    Add some config to cypress

---
+++ cypress/e2e/home.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../support/index.d.ts" />
+
+describe('Home Page', () => {
+  it('should render home', () => {
+    cy.visit('/')
+  })
+})
